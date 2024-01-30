43288cb62fc34323c9e8cc497ebe35d59f89952d
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-30 18:09:17 +0300

    Add default test

---
+++ src/test/java/com/andpostman/prometheusendpoint/PrometheusEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andpostman.prometheusendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusEndpointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
